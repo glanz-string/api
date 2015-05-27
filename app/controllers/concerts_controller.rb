00468d48955d731902fe2aff1f1f2823a8a9edd8
茨木暢仁 <niba@niba-MacBook-Pro.local>
2015-05-28 02:32:43 +0900

    リダイレクトを修正

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -24,7 +24,8 @@ class ConcertsController < ApplicationController
   private
     def authenticated_user!
       unless authenticated_user?
-        redirect_to 'signin'
+        #redirect_to '/signin'
+        redirect_to controller: 'signin' action: 'new'
       end
       
     end
