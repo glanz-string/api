9ed2e571ffe86943cea541625d0750549851cf83
Nobuhito Ibaraki <niba1122@gmail.com>
2015-06-24 16:52:16 +0900

    ログイン時のindex/indexの飛び先を修正

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 class IndexController < ApplicationController
   def index
     if authenticated_user?
-      redirect_to controller: 'concerts', action: 'show'
+      redirect_to controller: 'concerts', action: 'index'
     else
 #      render 'sessions/new'
       redirect_to controller: 'sessions', action: 'new'
